317ddc6ad5e461cf6253eb0d849700d84a518537
1PixelDemon1 <pxldem@gmail.com>
2022-08-26 22:04:59 +0500

    real Respond

---
+++ AiryBot/AiryBot/Activity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiryBot
+{
+    internal class Activity
+    {
+        public delegate bool act(List<string> parametres);
+
+        private string keyword;
+        private List<string> parametres;
+        private act action;
+
+        public Activity(string keyword_, List <string> parametres_, act action_) {
+            keyword = keyword_;
+            parametres = parametres_;
+            action = action_;
+        }
+        public bool perform() => action(parametres);
+
+        public string getKeyword() => keyword;
+    }
+}
--- AiryBot/AiryBot/Program.cs
+++ AiryBot/AiryBot/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using DSharpPlus;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace AiryBot
 {
     class Bot
     {
+
+        static private Respond respond = new Respond();
+
         static void Main()
         {
             MainAsync().GetAwaiter().GetResult();
@@ -13,17 +17,33 @@ namespace AiryBot
 
         static async Task MainAsync()
         {
+
+   
+
+
+
+
+            
+            respond.addActivity(new Activity("привет", new List<string>(), ((i) => { Console.WriteLine("Привет"); return false; })));
+
             var discord = new DiscordClient(new DiscordConfiguration()
 
             {
                 Token = GetToken(),
                 TokenType = TokenType.Bot,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
             });
 
             discord.MessageCreated += async (s, e) =>
             {
-                if (e.Message.Content.ToLower().StartsWith("айри!"))
-                    await e.Message.RespondAsync("Привет!");
+                if (respond.isActivity(e.Message.Content)) {
+                    respond.performActivity(e.Message.Content);
+
+                } 
+
+
+                //if (e.Message.Content.ToLower().StartsWith("айри!"))
+                //    await e.Message.RespondAsync("Привет!");
 
             };
 
@@ -38,7 +58,6 @@ namespace AiryBot
             StreamReader reader = new StreamReader("token.json");
                 string text = reader.ReadLine();
             // Returns token of Discord Bot. 
-            // Here was Ivan.
             return text;
         }
     }
