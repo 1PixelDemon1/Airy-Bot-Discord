4242680897f84026eace4435d0612a6294194308
1PixelDemon1 <pxldem@gmail.com>
2022-08-26 14:16:54 +0500

    Init commit

---
+++ AiryBot/AiryBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace AiryBot
+{
+    class Bot
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+
+            {
+                Token = GetToken(),
+                TokenType = TokenType.Bot,
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("айри!"))
+                    await e.Message.RespondAsync("Привет!");
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+
+
+        static string GetToken()
+        {
+            StreamReader reader = new StreamReader("token.json");
+                string text = reader.ReadLine();
+
+            return text;
+        }
+    }
+}
